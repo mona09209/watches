bf6c4823531eacc237fb66203a9ffa815af9eb28
Mona Mohamed <71096287+mona09209@users.noreply.github.com>
2025-05-25 07:38:39 +0200

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,10 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6832963343a2a8190b59e9b4/1is2plerm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
